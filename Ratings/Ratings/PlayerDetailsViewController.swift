8f6daffa421c69994862f9f3e96995aa324f707d
MaximeD <maxime.delilez@gmail.com>
2018-03-02 22:00:40 +0100

    Tutorial Part 2 Done

---
+++ Ratings/Ratings/PlayerDetailsViewController.swift
@@ -0,0 +1,76 @@
+//
+//  PlayerDetailsViewController.swift
+//  Ratings
+//
+//  Created by Maxime Delilez on 02-03-18.
+//  Copyright © 2018 Fontys. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailsViewController: UITableViewController {
+    
+    // MARK: - Properties
+    var player: Player?
+    
+    var game: String = "Chess" {
+        didSet {
+            detailLabel.text = game
+        }
+    }
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    // MARK: - Initializers
+    required init?(coder aDecoder: NSCoder) {
+        print("init PlayerDetailsViewController")
+        super.init(coder: aDecoder)
+    }
+    
+    deinit {
+        print("deinit PlayerDetailsViewController")
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SavePlayerDetail",
+            let playerName = nameTextField.text {
+            player = Player(name: playerName, game: game, rating: 1)
+        }
+        if segue.identifier == "PickGame",
+            let gamePickerViewController = segue.destination as? GamePickerViewController {
+            gamePickerViewController.selectedGame = game
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    }
+}
+
+// MARK: - IBActions
+extension PlayerDetailsViewController {
+    
+    @IBAction func unwindWithSelectedGame(segue: UIStoryboardSegue) {
+        if let gamePickerViewController = segue.source as? GamePickerViewController,
+            let selectedGame = gamePickerViewController.selectedGame {
+            game = selectedGame
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension PlayerDetailsViewController {
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            nameTextField.becomeFirstResponder()
+        }
+    }
+}
